901d1b771a9aaa6fc8cd410dcb289b313a4ad436
Daniil Menshikov <menshikoffda@yandex.ru>
2022-10-30 13:01:50 +0300

    Базовая часть

--- src/main/java/pom/MainPage.java
+++ src/main/java/pom/MainPage.java
@@ -1,2 +1,53 @@
-package pom;public class MainPage {
+package pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class MainPage {
+
+    private final String url = "https://qa-scooter.praktikum-services.ru";
+    private final WebDriver driver;
+    //Плашка с вопросом
+    private By questionPanelFromListOfQuestions = By.className("accordion__heading");
+    //Плашака с ответом
+    private By answerPanelFromListOfQuestions = By.className("accordion__panel");
+    //Верхняя кнопка заказа
+    private By topOrderButton = By.className("Button_Button__ra12g");
+    //Нижняя кнопка заказа
+    private By middleOrderButton = By.xpath(".//button[contains(@class, 'Button_Middle__1CSJM')]");
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+    public MainPage open() {
+        driver.get(url);
+        return this;
+    }
+    public MainPage scrollToTheSectionFaq() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+        return this;
+    }
+    public void pushQuestionPanelFromListOfQuestions(int number) {
+        driver.findElements(questionPanelFromListOfQuestions).get(number).click();
+    }
+    public String getTextOfAnswerOnQuestion(int number) {
+        return driver.findElements(answerPanelFromListOfQuestions).get(number).getText();
+    }
+    public WebDriver openOrderForm(String button) {
+        if (button == "top") {
+        driver.findElement(topOrderButton).click();}
+        else {
+            scrollToTheSectionFaq();
+            driver.findElement(middleOrderButton).click();}
+        return driver;
+    }
 }
--- src/main/java/pom/OrderForm.java
+++ src/main/java/pom/OrderForm.java
@@ -1,4 +1,86 @@
 package pom;
 
-public class OderForm {
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class OrderForm {
+
+    private final WebDriver driver;
+    private final By nameInput = By.xpath(".//input[@placeholder='* Имя']");
+    private final By familyNameInput = By.xpath(".//input[@placeholder='* Фамилия']");
+    private final By addressDeliveryInput = By.xpath(".//input[@placeholder='* Адрес: куда привезти заказ']");
+    private final By metroStationInput = By.xpath(".//input[@placeholder='* Станция метро']");
+    private final By clientPhoneNumberInput = By.xpath(".//input[@placeholder='* Телефон: на него позвонит курьер']");
+    private final By buttonFurther = By.xpath(".//button[text()='Далее']");
+    private final By deliveryDateInput = By.xpath(".//input[@placeholder='* Когда привезти самокат']");
+    private final By numberOfDaysToRentInput = By.xpath(".//div[@class='Dropdown-placeholder']");
+    private final By numberOfDaysToRentMenu =By.xpath(".//div[@class='Dropdown-menu']");
+    private final By numberOfDaysToRentOption = By.xpath(".//div[@class='Dropdown-option']");
+    private final By colourSamokatCheckBox = By.id("black");
+    private final By buttonOrder = By.xpath(".//div[@class='Order_Buttons__1xGrp']/button[text()='Заказать']");
+    private final By buttonYesToConfirmOrder = By.xpath(".//div[@class='Order_Modal__YZ-d3']//button[text()='Да']");
+    private final By headerOrderIsComplete = By.xpath(".//div[@class='Order_ModalHeader__3FDaJ']");
+
+    public OrderForm(WebDriver driver) {
+        this.driver = driver;
+    }
+    public WebElement getHeaderOrderIsComplete() {
+        return new WebDriverWait(driver, Duration.ofSeconds(3)).until(
+                ExpectedConditions.presenceOfElementLocated(headerOrderIsComplete));
+    }
+    public void pushMetroStationInput() {
+    driver.findElement(metroStationInput).click();
+    }
+    public void fillInName(String text) {
+        driver.findElement(nameInput).sendKeys(text);
+    }
+    public void fillInFamilyName(String text) {
+        driver.findElement(familyNameInput).sendKeys(text);
+    }
+    public void fillInAddressDelivery(String text) {
+        driver.findElement(addressDeliveryInput).sendKeys(text);
+    }
+    public void fillInMetroStationName(String metroStationName) {
+        pushMetroStationInput();
+        driver.findElement(metroStationInput).sendKeys(metroStationName);
+        new WebDriverWait(driver, Duration.ofSeconds(1));
+        driver.findElement(metroStationInput).sendKeys(Keys.ARROW_DOWN, Keys.ENTER);
+    }
+    public void fillInClientPhoneNumber(String text) {
+        driver.findElement(clientPhoneNumberInput).sendKeys(text);
+    }
+    public void pushButtonFurther() {
+        driver.findElement(buttonFurther).click();
+    }
+    public void fillInDeliveryDate() {
+        Calendar date = new GregorianCalendar();
+        date.add(Calendar.DAY_OF_MONTH, 3);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        driver.findElement(deliveryDateInput).sendKeys(formatter.format(date.getTime()), Keys.ENTER);
+    }
+    public void pushNumberOfDaysToRentInput() {
+        driver.findElement(numberOfDaysToRentInput).click();
+    }
+    public void selectNumberOfDaysTORentOption() {
+        new WebDriverWait(driver, Duration.ofSeconds(2)).until(driver ->
+                driver.findElement(numberOfDaysToRentMenu).getAttribute("aria-expanded").equals("true"));
+        driver.findElements(numberOfDaysToRentOption).get(2).click();
+    }
+    public void tickColourSamokatCheckBox() {
+        driver.findElement(colourSamokatCheckBox).click();
+    }
+    public void pushButtonOrder() {
+        driver.findElement(buttonOrder).click();
+    }
+    public void pushButtonYesToConfirmOrder() {
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(
+                driver.findElement(buttonYesToConfirmOrder))).click();
+    }
 }
